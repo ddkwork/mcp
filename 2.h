313038bb438aa4db6c876c954ed8e2a75a39000d
Admin <2762713521@qq.com>
2025-05-22 09:21:07 +0800

    抓包准备

--- 2.h
+++ 2.h
@@ -24,3 +24,24 @@ typedef enum
 int add (int a, int b){
     return a+b;
 };
+
+typedef unsigned char (*CHECK_VMX_OPERATION)();
+#define MAX_PATH 260
+typedef unsigned __int64   UINT64, *PUINT64;
+
+typedef struct _CR3_TYPE
+{
+    union
+    {
+        UINT64 Flags;
+
+        struct
+        {
+            UINT64 Pcid : 12;
+            UINT64 PageFrameNumber : 36;
+            UINT64 Reserved1 : 12;
+            UINT64 Reserved_2 : 3;
+            UINT64 PcidInvalidate : 1;
+        } Fields;
+    };
+} CR3_TYPE, *PCR3_TYPE;
